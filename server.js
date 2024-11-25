f7aacbb0912a9e6dde9d113c94fb084587045027
raifzakh <zakhraraif10@gmail.com>
2024-11-24 23:34:22 -0500

    Checking port connection

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
